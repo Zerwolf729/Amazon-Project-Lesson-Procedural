28ce4cf9a5d87339c6273dbf86f6d8e22e4f6df2
Zerwolf729 <samueljuliank29@gmail.com>
2025-05-10 21:11:19 +0800

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,22 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity:2
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity:2
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
 
-export function addToCart (productId) {
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+export function addToCart(productId) {
   let matchingItem;
     
-  cart.forEach((item) => {
+  cart.forEach((cartItem) => {
     if(productId === cartItem.productId) {
       matchingItem = cartItem;
     }
@@ -23,11 +30,12 @@ export function addToCart (productId) {
       quantity: 1
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
   const newCart = [];
-  
+
   cart.forEach((cartItem) => {
     if (cartItem.productId !== productId) {
       newCart.push(cartItem);
@@ -35,5 +43,7 @@ export function removeFromCart(productId) {
   }) 
 
   cart = newCart;
+
+  saveToStorage();
 }
 
