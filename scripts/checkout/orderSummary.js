ba92bdb3e610239a6f9a2c7df6ded310a3275740
Zerwolf729 <samueljuliank29@gmail.com>
2025-05-31 19:53:19 +0800

    Finish Insert Course 16

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -23,7 +23,9 @@ export function renderOrderSummary() {
 
     cartSummaryHTML += 
     `
-      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="cart-item-container 
+      js-cart-item-container
+      js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: ${dateString}
         </div>
@@ -39,19 +41,26 @@ export function renderOrderSummary() {
             <div class="product-price">
               $${formatCurrency(matchingProduct.priceCents)}
             </div>
-            <div class="product-quantity">
+            <div class="product-quantity
+            js-product-quantity-${matchingProduct.id}">
               <span>
                 Quantity: <span class="quantity-label">${cartItem.quantity}</span>
               </span>
-              <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
+              <span class="update-quantity-link 
+              link-primary 
+              js-update-link" data-product-id="${matchingProduct.id}">
                 Update
               </span>
-              <input class="quantity-input js-quantity-input-${matchingProduct.id}">
-              <span class="save-quantity-link link-primary js-save-link"
-                data-product-id="${matchingProduct.id}">
+              <input class="quantity-input 
+              js-quantity-input-${matchingProduct.id}">
+              <span class="save-quantity-link 
+              link-primary 
+              js-save-link" data-product-id="${matchingProduct.id}">
                 Save
               </span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              <span class="delete-quantity-link link-primary 
+              js-delete-link
+              js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                 Delete
               </span>
             </div>
@@ -111,6 +120,10 @@ export function renderOrderSummary() {
       link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+        container.remove();
         
         updateCartQuantity();
         renderCheckoutHeader();
